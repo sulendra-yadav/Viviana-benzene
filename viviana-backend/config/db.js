b37c9a6a3d2f8e9039daafbf0a6d86820f32720f
sulendra yadav <yadavsulendra123@gmail.com>
2025-10-23 17:19:33 +0530

    Update db.js

--- viviana-backend/config/db.js
+++ viviana-backend/config/db.js
@@ -7,6 +7,9 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+  ca: process.env.DB_SSL_CA
+},
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
